d3445ca86e90394563d8a3a6a66548362a9ec150
gamezftw <gamezftw@yahoo.com>
2024-08-24 10:28:30 +0200

    update install script and playbook

--- ansible-new-pc.sh
+++ ansible-new-pc.sh
@@ -1,2 +1,2 @@
 #/usr/bin/env bash
-ansible-playbook workstation.yml --tags untagged,nvidia,pipewire,tplink-usb $1
+ansible-playbook workstation.yml --tags untagged,nvidia,pipewire,tplink-usb,docker,aws,openvpn $1
